f83f021bd55e3a9132aa78c0966ad34653d3a251
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-12-08 16:00:26 -0500

    flexbox formatting, css updates, ygoprodeck api integration begun

--- src/components/Decklist/Decklist.js
+++ src/components/Decklist/Decklist.js
@@ -10,6 +10,7 @@ function Decklist() {
   const [decklist, setDecklist] = useState([
     {...blankCardEntry}
   ]);
+  const [searchResults, setSearchResults] = useState([]);
 
   const addCardEntryRow = () => {
     setDecklist([...decklist, {...blankCardEntry}]);
@@ -19,6 +20,8 @@ function Decklist() {
     const updatedDecklist = [...decklist];
     updatedDecklist[event.target.dataset.index].name = event.target.value;
     setDecklist(updatedDecklist);
+    const results = searchCard(event.target.value);
+    setSearchResults(results);
   }  
   
   const handleCopyChange = (event) => {
@@ -27,28 +30,54 @@ function Decklist() {
     setDecklist(updatedDecklist);
   }
 
+  const searchCard = (search) => {
+    const url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php';
+    const searchParam = encodeURI(search);
+    fetch(`${url}?fname=${searchParam}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => {
+        const data = responseJson.data.map(element => element.name);
+        console.log(data);
+        return data;
+      })
+      .catch(err => {
+      })
+  }
+
   return (
     <>
-      <form className="decklist-form">
-        <legend>Decklist</legend>
-        {
-          decklist.map((value, index) => {
-            const nameId = `name-${index}`;
-            const copiesId = `copies-${index}`;
-            return (
-              <div key={`row-${index}`}>
-                <label htmlFor={nameId}>Card Name: </label>
-                <input type="text" name={nameId} data-index={index} id={nameId} className="cardname" value={decklist[index].name} onChange={handleCardChange} />
-                <label htmlFor={nameId}>Copies: </label>
-                <input type="number" name={copiesId} data-index={index} id={copiesId} className="copies" value={decklist[index].copies} onChange={handleCopyChange} min="1" max="3" />
-                {index === decklist.length - 1 && (decklist[index].copies >= 1 && decklist[index].copies <= 3) && decklist[index].name.length > 0 &&
-                  <input type="button" value="Add Card" onClick={addCardEntryRow} />
-                }
-              </div>
-            );
-          })
-        }
-      </form>
+      <div className="deckmaker-container">
+        <form className="decklist-form" autoComplete="off">
+          <legend>Decklist</legend>
+          {
+            decklist.map((value, index) => {
+              const nameId = `name-${index}`;
+              const copiesId = `copies-${index}`;
+              return (
+                <div className="cardname-copies-container" key={`row-${index}`}>
+                  <div className="cardname-copies-column">
+                    <label htmlFor={nameId}>Card Name: </label>
+                    <input type="text" name={nameId} data-index={index} id={nameId} className="cardname" value={decklist[index].name} onChange={handleCardChange} />
+                    <label htmlFor={nameId}>Copies: </label>
+                    <input type="number" name={copiesId} data-index={index} id={copiesId} className="copies" value={decklist[index].copies} onChange={handleCopyChange} min="1" max="3" />
+                  </div>
+                  <div className="add-button-column">  
+                    {index === decklist.length - 1 && (decklist[index].copies >= 1 && decklist[index].copies <= 3) && decklist[index].name.length > 0 ?
+                        <input className="add-card-button" type="button" value="Add Card" onClick={addCardEntryRow} /> :
+                        <input className="add-button-column hidden" type="button" value="Add Card" onClick={addCardEntryRow} />
+                    }
+                  </div>
+                </div>
+              );
+            })
+          }
+        </form>
+      </div>
       {
         decklist.map((value, index) => {
           let cardLine;
@@ -58,7 +87,7 @@ function Decklist() {
             cardLine = <></>
           }
           return (
-            <div key={`cardLine-${value}=${index}`}>
+            <div className="decklist-container" key={`cardLine-${value}=${index}`}>
               {cardLine}
             </div>
           );
